8f6ebfa26b9149dab9d61b79989127115a0a9fd4
pu6er <djakobstv@gmail.com>
2023-09-22 16:18:44 +0300

    feat: add modal create task form

--- src/components/tasks-list/Tasks.tsx
+++ src/components/tasks-list/Tasks.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 
-import { useGetTasksQuery } from "../../store/api/api";
 import TaskGroup from "../task-group/TaskGroup";
 import styles from "./Tasks.module.css";
-import Modal from "react-modal";
 import { useState } from "react";
 import CreateTask from "../create-task/CreateTask";
 
@@ -11,34 +9,33 @@ const TaskSections = ["To Do", "In Progress", "Need Review", "Done"];
 
 const TasksList = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const handleModal = () => {
+  const [selectedSection, setSelectedSection] = useState("");
+  const handleModalOpen = (selectedSection: string) => {
+    setSelectedSection(selectedSection);
     setModalIsOpen((prevState) => !prevState);
   };
+  const handleModalClose = () => {
+    setModalIsOpen((prevState) => !prevState);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.group}>
         {TaskSections.map((section) => (
           <div>
             <h2>{section}</h2>
-            <button onClick={handleModal} style={{ marginBottom: "10px" }}>
+            <button
+              onClick={() => handleModalOpen(section)}
+              style={{ marginBottom: "10px" }}
+            >
               Add new Task
             </button>
-            <Modal
-              isOpen={modalIsOpen}
-              contentElement={() => <CreateTask handleModal={handleModal} />}
-              //   className={styles.modal}
-              style={{
-                content: {
-                  backgroundColor: "#0000",
-                  position: "absolute",
-                  left: "100px",
-                },
-                overlay: {
-                  position: "fixed",
-                },
-              }}
-              //   overlayClassName={styles.overlay}
-            ></Modal>
+            {modalIsOpen && section == selectedSection && (
+              <CreateTask
+                handleModal={handleModalClose}
+                sectionStatus={selectedSection}
+              />
+            )}
             <TaskGroup status={section} />
           </div>
         ))}
