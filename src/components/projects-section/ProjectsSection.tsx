70b1d8e99fa5e17524ae20cd9d5b031949713e44
pu6er <djakobstv@gmail.com>
2023-10-01 23:25:49 +0300

    add: add project section

---
+++ src/components/projects-section/ProjectsSection.tsx
@@ -0,0 +1,11 @@
+import ProjectItem from "../project-item/ProjectItem";
+
+const ProjectsSection = () => {
+  return (
+    <div>
+      <ProjectItem />
+    </div>
+  );
+};
+
+export default ProjectsSection;
