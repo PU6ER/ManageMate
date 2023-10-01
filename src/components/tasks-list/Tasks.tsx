5076ea1e925811ccafaf85386929e14d567a9e0e
pu6er <djakobstv@gmail.com>
2023-10-01 23:28:33 +0300

    feat: add color point to task group

--- src/components/tasks-list/Tasks.tsx
+++ src/components/tasks-list/Tasks.tsx
@@ -2,14 +2,24 @@ import * as React from "react";
 
 import TaskGroup from "../task-group/TaskGroup";
 import styles from "./Tasks.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateTask from "../create-task/CreateTask";
+import { useGetTasksByStatusQuery } from "../../store/api/task.api";
+import { useGetTasksQuery } from "../../store/api/api";
 
-const TaskSections = ["To Do", "In Progress", "Need Review", "Done"];
+const TaskSections = [
+  { name: "To Do", color: "#ff8179" },
+  { name: "In Progress", color: "#365eff" },
+  { name: "Need Review", color: "#ffbd38" },
+  { name: "Done", color: "#11bf95" },
+];
 
 const TasksList = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedSection, setSelectedSection] = useState("");
+  const [context, setContext] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(0);
+  const { isLoading, data } = useGetTasksQuery(null);
   const handleModalOpen = (selectedSection: string) => {
     setSelectedSection(selectedSection);
     setModalIsOpen((prevState) => !prevState);
@@ -23,20 +33,49 @@ const TasksList = () => {
       <div className={styles.group}>
         {TaskSections.map((section) => (
           <div>
-            <h2>{section}</h2>
-            <button
-              onClick={() => handleModalOpen(section)}
-              style={{ marginBottom: "10px" }}
+            <div
+              style={{
+                marginBottom: "15px",
+                display: "flex",
+                alignItems: "center",
+                justifyItems: "center",
+              }}
             >
-              Add new Task
+              <div
+                className={styles.color}
+                style={{ backgroundColor: `${section.color}` }}
+              ></div>
+              <span
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                {section.name}
+              </span>
+              <div className={styles.taskCount}>
+                {data?.filter((task) => task.status === section.name).length}
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={() => handleModalOpen(section.name)}
+              className={styles.delete}
+            >
+              + Add new Task
             </button>
-            {modalIsOpen && section == selectedSection && (
+            {modalIsOpen && section.name == selectedSection && (
               <CreateTask
                 handleModal={handleModalClose}
                 sectionStatus={selectedSection}
               />
             )}
-            <TaskGroup status={section} />
+            <TaskGroup
+              status={section.name}
+              context={context}
+              setContext={setContext}
+              selectedTask={selectedTask}
+              setSelectedTask={setSelectedTask}
+            />
           </div>
         ))}
       </div>
