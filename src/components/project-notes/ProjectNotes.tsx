4ada3cc3d178c8588b9cfd28a854c4915d4dfb2e
pu6er <djakobstv@gmail.com>
2023-10-07 00:14:01 +0300

    feat: add project notes components

---
+++ src/components/project-notes/ProjectNotes.tsx
@@ -0,0 +1,14 @@
+import styles from "./ProjectNotes.module.css";
+
+const ProjectNotes = () => {
+  return (
+    <div className={styles.container}>
+      <span>Project Notes</span>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  );
+};
+
+export default ProjectNotes;
