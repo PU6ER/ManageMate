30785624bbac907131b480ee5c3716adc46ac51c
pu6er <djakobstv@gmail.com>
2023-10-07 00:23:15 +0300

    feat: updated children

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,3 +1,4 @@
+import Project from "../components/project/Project";
 import Sidebar from "../components/sidebar/Sidebar";
 import TasksList from "../components/tasks-list/Tasks";
 import styles from "./ProjectPage.module.css"
@@ -6,7 +7,7 @@ const ProjectPage = () => {
   return (
     <div className={styles.container}>
       
-      <TasksList />
+      <Project />
     </div>
   );
 };
