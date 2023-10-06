f00825a65f35dd6a65ee81543665cfa8d80bb4cc
pu6er <djakobstv@gmail.com>
2023-10-07 00:23:56 +0300

    feat: add project related methods

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,24 +1,33 @@
+import { IProject } from "./../../types/project.types";
 import { ITask } from "./../../types/task.types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URL = "http://localhost:4200/tasks";
+const API_URL = "http://localhost:4200";
 
 export const api = createApi({
   reducerPath: "api",
-  tagTypes: ["Task"],
+  tagTypes: ["Task", "Project"],
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
   }),
   endpoints: (builder) => ({
     getTasks: builder.query<ITask[], null>({
-      query: () => `/?_sort=id&_order=desc`,
+      query: () => `/tasks/?_sort=id&_order=desc`,
       providesTags: (result, error) => [
         {
           type: "Task",
         },
       ],
     }),
+    getProjects: builder.query<IProject[], null>({
+      query: () => `/projects/?_sort=id&_order=desc`,
+      providesTags: () => [
+        {
+          type: "Project",
+        },
+      ],
+    }),
   }),
 });
 
-export const { useGetTasksQuery } = api;
+export const { useGetTasksQuery, useGetProjectsQuery } = api;
