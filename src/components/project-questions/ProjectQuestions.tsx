c8f686cff624c1e5d621e2dc165794761e3c54ca
pu6er <djakobstv@gmail.com>
2023-10-07 00:15:50 +0300

    feat: add project questions tab

---
+++ src/components/project-questions/ProjectQuestions.tsx
@@ -0,0 +1,14 @@
+import styles from "./ProjectQuestions.module.css";
+
+const ProjectQuestions = () => {
+  return (
+    <div className={styles.container}>
+      <span>Project Questions</span>
+      <span>Q</span>
+      <span>Q</span>
+      <span>Q</span>
+    </div>
+  );
+};
+
+export default ProjectQuestions;
