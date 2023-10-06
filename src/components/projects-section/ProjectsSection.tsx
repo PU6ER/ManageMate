490dc92267a14ad537ace71094da0668879a2df2
pu6er <djakobstv@gmail.com>
2023-10-07 00:17:59 +0300

    feat: change projects render

--- src/components/projects-section/ProjectsSection.tsx
+++ src/components/projects-section/ProjectsSection.tsx
@@ -1,9 +1,22 @@
+import { useActions } from "../../hooks/useActions";
+import { useProjects } from "../../hooks/useProjects";
+import { useGetProjectsQuery } from "../../store/api/api";
 import ProjectItem from "../project-item/ProjectItem";
 
 const ProjectsSection = () => {
+  const { data, isLoading } = useGetProjectsQuery(null);
+  const { setProjectId } = useActions();
   return (
     <div>
-      <ProjectItem />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        data?.map((project) => (
+          <div className="">
+            <span onClick={() => setProjectId(project.id)}>{project.name}</span>
+          </div>
+        ))
+      )}
     </div>
   );
 };
