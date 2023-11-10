19a442f63d7cdf56afd06c5980f3c3cedbc669fa
pu6er <djakobstv@gmail.com>
2023-11-10 19:01:23 +0300

    feat: add new sections and logos

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,17 +1,61 @@
 import SidebarSection from "../sidebar-section/SidebarSection";
 import styles from "./Sidebar.module.css";
+import {
+  LuPanelLeft,
+  LuLayoutGrid,
+  LuTimer,
+  LuCalendarDays,
+  LuBarChart3,
+} from "react-icons/lu";
+import { IconContext } from "react-icons/lib";
+import Logo from "../../managematelogo.png";
+import { useSidebar } from "../../hooks/useSidebar";
+import { useActions } from "../../hooks/useActions";
 
 const Sidebar = () => {
+  const { sidebar } = useSidebar();
+  const { setSidebarSection, setProjectId, setStateToInitial } = useActions();
   return (
-    <div className={styles.sidebar}>
+    <div
+      className={sidebar.length === 0 ? styles.sidebarDisabled : styles.sidebar}
+    >
       <div className={styles.left}>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>5</span>
+        <img src={Logo} alt="" onClick={() => setSidebarSection("abo")} />
+        <div className={styles.icon}>
+          <LuLayoutGrid size="1.5em" />
+        </div>
+        <div
+          className={
+            sidebar[0] === "projects" ? styles.iconActive : styles.icon
+          }
+        >
+          <IconContext.Provider
+            value={sidebar[0] === "projects" ? { color: "#365eff" } : {}}
+          >
+            <LuPanelLeft
+              size="1.5em"
+              onClick={() => setSidebarSection("projects")}
+            />
+          </IconContext.Provider>
+        </div>
+        <div
+          className={sidebar[0] === "timer" ? styles.iconActive : styles.icon}
+        >
+          <IconContext.Provider
+            value={sidebar[0] === "timer" ? { color: "#365eff" } : {}}
+          >
+            <LuTimer size="1.5em" onClick={() => setSidebarSection("timer")} />
+          </IconContext.Provider>
+        </div>
+        <div className={styles.icon}>
+          <LuCalendarDays size="1.5em" className={styles.icon} />
+        </div>
+        <div className={styles.icon}>
+          <LuBarChart3 size="1.5em" className={styles.icon} />
+        </div>
       </div>
-      <div>
+
+      <div className={styles.right}>
         <SidebarSection />
       </div>
     </div>
