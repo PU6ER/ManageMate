a01029c7641ddc98816be2b5aa748fbfef188ed5
pu6er <djakobstv@gmail.com>
2023-11-10 18:49:06 +0300

    feat: add Timer page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,15 @@ import TasksList from "./tasks-list/Tasks";
 import ProjectPage from "../pages/ProjectPage";
 import Sidebar from "./sidebar/Sidebar";
 import styles from "./App.module.css"
+import { useProjects } from "../hooks/useProjects";
+import TimerPage from "../pages/timer/TimerPage";
 // const userId= 1
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [queryTerm, setQueryTerm] = useState("");
+  
   // const { isLoading, data } = useGetRecipesQuery(queryTerm);
   // undefined,
   // { skip: !userId });
@@ -22,6 +25,7 @@ function App() {
       {/* <CreateTask /> */}
       <Sidebar />
       <ProjectPage />
+      <TimerPage />
     </div>
   );
 }
