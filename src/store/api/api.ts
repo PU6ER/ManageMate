3a8a68a62a93d0c71c04f41fb9e53b3ee1b3051c
pu6er <djakobstv@gmail.com>
2023-11-10 19:22:50 +0300

    refactor: redu api to new apiUrl

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,33 +1,55 @@
-import { IProject } from "./../../types/project.types";
+import { INote, IProject, IQuestion } from "./../../types/project.types";
 import { ITask } from "./../../types/task.types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URL = "http://localhost:4200";
+const API_URL = "http://localhost:5555/api";
 
 export const api = createApi({
-  reducerPath: "api",
-  tagTypes: ["Task", "Project"],
+  // reducerPath: "api",
+  tagTypes: ["Task", "Project", "Note", "Question", "Subtask"],
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
   }),
   endpoints: (builder) => ({
-    getTasks: builder.query<ITask[], null>({
-      query: () => `/tasks/?_sort=id&_order=desc`,
-      providesTags: (result, error) => [
+    getTasks: builder.query<ITask[], number>({
+      query: (id) => `/projects/${id}/tasks`,
+      providesTags: (result, error, id) => [
         {
           type: "Task",
+          id: `${id}`,
         },
       ],
     }),
     getProjects: builder.query<IProject[], null>({
-      query: () => `/projects/?_sort=id&_order=desc`,
+      query: () => `/projects`,
       providesTags: () => [
         {
           type: "Project",
         },
       ],
     }),
+    getNotes: builder.query<INote[], number>({
+      query: (id) => `/projects/${id}/notes`,
+      providesTags: () => [
+        {
+          type: "Note",
+        },
+      ],
+    }),
+    getQuestions: builder.query<IQuestion[], number>({
+      query: (id) => `/projects/${id}/questions`,
+      providesTags: () => [
+        {
+          type: "Question",
+        },
+      ],
+    }),
   }),
 });
 
-export const { useGetTasksQuery, useGetProjectsQuery } = api;
+export const {
+  useGetTasksQuery,
+  useGetProjectsQuery,
+  useGetNotesQuery,
+  useGetQuestionsQuery,
+} = api;
