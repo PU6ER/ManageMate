5a2460945e2ca00c193527e8faa9ccaa41139982
pu6er <djakobstv@gmail.com>
2023-11-10 18:59:07 +0300

    feat: update projects section

--- src/components/projects-section/ProjectsSection.tsx
+++ src/components/projects-section/ProjectsSection.tsx
@@ -1,19 +1,37 @@
 import { useActions } from "../../hooks/useActions";
 import { useProjects } from "../../hooks/useProjects";
 import { useGetProjectsQuery } from "../../store/api/api";
+import styles from "./ProjectsSection.module.css";
+import Loader from "../loader/Loader";
 import ProjectItem from "../project-item/ProjectItem";
+import { Emoji } from "emoji-picker-react";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const ProjectsSection = () => {
-  const { data, isLoading } = useGetProjectsQuery(null);
+  const { data, isLoading, isError, error, } = useGetProjectsQuery(null);
   const { setProjectId } = useActions();
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <span>Projects</span>
+      </div>
       {isLoading ? (
-        <div>Loading...</div>
+        <Loader style={{}} />
       ) : (
         data?.map((project) => (
-          <div className="">
-            <span onClick={() => setProjectId(project.id)}>{project.name}</span>
+          <div
+            className={styles.project}
+            onClick={() => setProjectId(project.id)}
+          >
+            <div className={styles.projectLeft}>
+              <div className={styles.emoji}>
+                {/* <Emoji unified={project.image} size={15} /> */}
+              </div>
+              <span>{project.name}</span>
+            </div>
+            <div className={styles.projectRight}>
+              <BsThreeDotsVertical color="black" size="1em" />
+            </div>
           </div>
         ))
       )}
