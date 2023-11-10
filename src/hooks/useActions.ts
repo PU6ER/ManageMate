70f713437bba0d011e7381b91d9102e52abbe897
pu6er <djakobstv@gmail.com>
2023-11-10 19:15:51 +0300

    feat: add new reducer's actions

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -2,9 +2,13 @@ import { bindActionCreators } from "@reduxjs/toolkit";
 import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../store/projects/projects.slice";
+import { actions as sidebar } from "../store/sidebar/sidebar.slice";
+import { actions as task } from "../store/task/task.slice";
+
 const rootActions = {
   ...actions,
-  
+  ...sidebar,
+  ...task,
 };
 
 export const useActions = () => {
