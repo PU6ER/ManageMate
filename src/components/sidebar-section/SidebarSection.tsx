c1131ec46dd68861dfe04f63efc737ea2c151771
pu6er <djakobstv@gmail.com>
2023-11-10 19:04:47 +0300

    feat: add new features

--- src/components/sidebar-section/SidebarSection.tsx
+++ src/components/sidebar-section/SidebarSection.tsx
@@ -1,10 +1,18 @@
+import { useSidebar } from "../../hooks/useSidebar";
 import ProjectsSection from "../projects-section/ProjectsSection";
 import styles from "./SidebarSection.module.css";
 const SidebarSection = () => {
+  const { sidebar } = useSidebar();
   return (
-    <div className={styles.section}>
-      <ProjectsSection />
-    </div>
+    <>
+      {sidebar[0] !== "timer" && (
+        <div className={styles.section}>
+          {sidebar.length > 0 && sidebar[0] === "projects" && (
+            <ProjectsSection />
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
