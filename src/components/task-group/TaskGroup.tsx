ff60fc0df2beb9deb2c5d2ea185680b463cf5c7e
pu6er <djakobstv@gmail.com>
2023-11-10 19:12:45 +0300

    feat: add new functionality

--- src/components/task-group/TaskGroup.tsx
+++ src/components/task-group/TaskGroup.tsx
@@ -5,7 +5,18 @@ import styles from "./TaskGroup.module.css";
 import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { AiOutlineClose } from "react-icons/ai";
+import {
+  LuEye,
+  LuMessageCircle,
+  LuPaperclip,
+  LuListChecks,
+} from "react-icons/lu";
 import { useState } from "react";
+import { useProjects } from "../../hooks/useProjects";
+import { useActions } from "../../hooks/useActions";
+import { useTask } from "../../hooks/useTask";
+import { useGetSubTasksByIdQuery } from "../../store/api/subtask.api";
+import SubtaskButton from "../subtask-button/SubtaskButton";
 interface ITaskItem {
   status: string;
   context: boolean;
@@ -14,9 +25,18 @@ interface ITaskItem {
   setSelectedTask: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const TaskGroup = ({ status, context, setContext, selectedTask, setSelectedTask }: ITaskItem) => {
+const TaskGroup = ({
+  status,
+  context,
+  setContext,
+  selectedTask,
+  setSelectedTask,
+}: ITaskItem) => {
   const [deleteTask] = useDeleteTaskMutation();
-  const { isLoading, data,  } = useGetTasksQuery(null);
+  const { setTask } = useActions();
+  const { projects } = useProjects();
+  const { isLoading, data } = useGetTasksQuery(projects[0]);
+  const { task: taskSlice } = useTask();
 
   const handleContextOpen = (taskId: number) => {
     // if(context && selectedTask !== taskId) setContext(true)
@@ -34,13 +54,25 @@ const TaskGroup = ({ status, context, setContext, selectedTask, setSelectedTask 
         .map((task) => (
           <div className={styles.container}>
             <div className={styles.header}>
-              <h4>{task.name}</h4>
+              <div
+                className={
+                  task.group === "UX stages"
+                    ? styles.ux
+                    : task.group === "Branding"
+                    ? styles.branding
+                    : task.group === "Development"
+                    ? styles.dev
+                    : styles.taskGroup
+                }
+              >
+                <span>{task.group}</span>{" "}
+              </div>
 
               <button
                 className={styles.contextBtn}
                 onClick={() => handleContextOpen(task.id)}
               >
-                <BsThreeDotsVertical color="black" />
+                <BsThreeDotsVertical color="black" size="1.5em" />
               </button>
             </div>
             {/* <button onClick={() => deleteTask(task.id)}>Delete</button> */}
@@ -51,13 +83,16 @@ const TaskGroup = ({ status, context, setContext, selectedTask, setSelectedTask 
                     style={{ backgroundColor: "#fff" }}
                     onClick={handleContextClose}
                   >
-                    <AiOutlineClose />
+                    <AiOutlineClose size="1.5em" />
                   </button>
                 </div>
                 <div className={styles.contextItems}>
                   <button
                     className={styles.btn}
-                    onClick={() => deleteTask(task.id)}
+                    onClick={() => {
+                      deleteTask(task.id);
+                      setContext(false);
+                    }}
                   >
                     Delete
                   </button>
@@ -65,7 +100,37 @@ const TaskGroup = ({ status, context, setContext, selectedTask, setSelectedTask 
               </div>
             )}
             {/* <span>{task.name}</span> */}
-            <span className={styles.textDescr}>{task.description}</span>
+            <div className={styles.content}>
+              <h4>{task.name}</h4>
+              <span className={styles.textDescr}>{task.description}</span>
+              {/* <button
+                className={styles.subTasks}
+                onClick={() => setTask(task.id)}
+              >
+                <LuListChecks size="1.5em" />
+                <span>0/8</span>
+              </button> */}
+              <SubtaskButton taskId={task.id} />
+            </div>
+            <div className={styles.footerWrapper}>
+              <div className={styles.footer}>
+                <div>Memebers</div>
+                <div className={styles.footerIcons}>
+                  <div className={styles.footerIcon}>
+                    <LuEye />
+                    <span>2</span>
+                  </div>
+                  <div className={styles.footerIcon}>
+                    <LuMessageCircle />
+                    <span>3</span>
+                  </div>
+                  <div className={styles.footerIcon}>
+                    <LuPaperclip />
+                    <span>1</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
     </>
