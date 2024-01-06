85343d22237826690df37beb9a02c3f48c1cb891
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-01-06 20:13:23 +0300

    feat: update app file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,31 @@ import CreateTask from "./create-task/CreateTask";
 import TasksList from "./tasks-list/Tasks";
 import ProjectPage from "../pages/ProjectPage";
 import Sidebar from "./sidebar/Sidebar";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import { useProjects } from "../hooks/useProjects";
 import TimerPage from "../pages/timer/TimerPage";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import AuthPage from "../pages/auth/AuthPage";
+import { useUser } from "../hooks/useUser";
+import Project from "./project/Project";
+import UserPage from "../pages/user/UserPage";
 // const userId= 1
+
+// createBrowserRouter(createRoutesFromElements(<Route path="/" element={} />));
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [queryTerm, setQueryTerm] = useState("");
-  
+  const { user } = useUser();
+
   // const { isLoading, data } = useGetRecipesQuery(queryTerm);
   // undefined,
   // { skip: !userId });
@@ -23,9 +38,20 @@ function App() {
   return (
     <div className={styles.container}>
       {/* <CreateTask /> */}
-      <Sidebar />
-      <ProjectPage />
-      <TimerPage />
+      {user.length === 0 ? (
+        <AuthPage />
+      ) : (
+        <>
+          <Sidebar />
+          <Routes>
+            <Route path="/projects" element={<ProjectPage />}></Route>
+            <Route path="/timer" element={<TimerPage />}></Route>
+            <Route path="/user" element={<UserPage />}></Route>
+            {/* <ProjectPage /> */}
+            {/* <TimerPage /> */}
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
