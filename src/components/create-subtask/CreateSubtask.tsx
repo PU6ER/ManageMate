7249294f3247a16f354cf8bfb4c5eea78a8031fd
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-01-06 20:16:40 +0300

    feat: update create subtask functionality

--- src/components/create-subtask/CreateSubtask.tsx
+++ src/components/create-subtask/CreateSubtask.tsx
@@ -1,74 +1,75 @@
-import { useTask } from "../../hooks/useTask";
-import { ISubtaskData } from "../../types/task.types";
-import { useState } from "react";
-import { useCreateTaskMutation } from "../../store/api/task.api";
-import { ITaskData } from "../../types/task.types";
-import { AiOutlineClose } from "react-icons/ai";
-import styles from "./CreateSubtask.module.css";
-import { useProjects } from "../../hooks/useProjects";
-import { useCreateNoteMutation } from "../../store/api/notes.api";
-import { INoteData } from "../../types/project.types";
-import { useCreateSubtaskMutation } from "../../store/api/subtask.api";
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { useProjects } from '../../hooks/useProjects'
+import { useTask } from '../../hooks/useTask'
+import { useCreateSubtaskMutation } from '../../store/api/subtask.api'
+import { ISubtaskData } from '../../types/task.types'
+import styles from './CreateSubtask.module.css'
 const defaultValue: ISubtaskData = {
-  name: "",
-  description: "",
-  done: false,
-};
+	name: '',
+	description: '',
+	done: false,
+}
 
 interface TaskProps {
-  handleModal: () => void;
+	handleModal: () => void
 }
 
 export default function CreateSubtask({ handleModal }: TaskProps) {
-  const [subtask, setSubtask] = useState<ISubtaskData>(defaultValue);
-  const { projects } = useProjects();
-  const { task } = useTask();
-  const [createSubtask] = useCreateSubtaskMutation();
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	const [subtask, setSubtask] = useState<ISubtaskData>(defaultValue)
+	const { projects } = useProjects()
+	const { task } = useTask()
+	const [createSubtask] = useCreateSubtaskMutation()
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
 
-    createSubtask([subtask, projects[0], task[0]]).then(() => {
-      setSubtask(defaultValue);
-    });
-    setTimeout(() => handleModal(), 1000);
-  };
-  return (
-    <div>
-      <form onSubmit={handleSubmit} className={styles.container}>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <span style={{ fontWeight: "bold" }}>Create new note: </span>
-          <button style={{ backgroundColor: "#ffff" }} onClick={handleModal}>
-            <AiOutlineClose />
-          </button>
-        </div>
-        <label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={subtask.name}
-            onChange={(e) => setSubtask({ ...subtask, name: e.target.value })}
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            placeholder="Description"
-            value={subtask.description}
-            onChange={(e) =>
-              setSubtask({ ...subtask, description: e.target.value })
-            }
-          />
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            placeholder="Description"
-            // value={subtask.done}
-            checked={subtask.done}
-            onChange={(e) => setSubtask({ ...subtask, done: e.target.checked })}
-          />
-        </label>
-        {/* <label>
+		createSubtask([subtask, projects[0], task[0]]).then(() => {
+			setSubtask(defaultValue)
+		})
+		setTimeout(() => handleModal(), 1000)
+	}
+	return (
+		<motion.div
+			initial={{ y: -10 }}
+			animate={{ y: 0 }}
+			transition={{ duration: 0.5 }}
+		>
+			<form onSubmit={handleSubmit} className={styles.container}>
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<span style={{ fontWeight: 'bold' }}>Create new note: </span>
+					<button style={{ backgroundColor: '#ffff' }} onClick={handleModal}>
+						<AiOutlineClose />
+					</button>
+				</div>
+				<label>
+					<input
+						type='text'
+						placeholder='Name'
+						value={subtask.name}
+						onChange={e => setSubtask({ ...subtask, name: e.target.value })}
+					/>
+				</label>
+				<label>
+					<input
+						type='text'
+						placeholder='Description'
+						value={subtask.description}
+						onChange={e =>
+							setSubtask({ ...subtask, description: e.target.value })
+						}
+					/>
+				</label>
+				<label>
+					<input
+						type='checkbox'
+						placeholder='Description'
+						// value={subtask.done}
+						checked={subtask.done}
+						onChange={e => setSubtask({ ...subtask, done: e.target.checked })}
+					/>
+				</label>
+				{/* <label>
           <input
             type="text"
             placeholder="Status"
@@ -76,10 +77,10 @@ export default function CreateSubtask({ handleModal }: TaskProps) {
             onChange={(e) => setTask({ ...task, status: e.target.value })}
           />
         </label> */}
-        <button type="submit" className={styles.createBtn}>
-          Create
-        </button>
-      </form>
-    </div>
-  );
+				<button type='submit' className={styles.createBtn}>
+					Create
+				</button>
+			</form>
+		</motion.div>
+	)
 }
